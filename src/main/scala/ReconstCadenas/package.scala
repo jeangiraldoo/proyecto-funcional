02da69dee1f86b26b75ac8ade2becb64397be1bc
Jean Giraldo <jeann.giraldo@gmail.com>
2025-06-01 23:01:02 -0500

    feat: add reconstruirCadenaIngenuaPar

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -3,9 +3,7 @@ import Common ._
 
 package object ReconstCadenas {
   val alfabeto = Seq('a', 'c', 'g', 't')
-
-
-
+  
   def reconstruirCadenaIngenua(n: Int, oraculo: Oraculo): Seq[Char] = {
     def generateAll(length: Int): Seq[Seq[Char]] = {
       if (length == 0) Seq(Seq())
@@ -20,6 +18,33 @@ package object ReconstCadenas {
       candidate <- generateAll(len)
       if result.isEmpty && oraculo(candidate) && candidate.length == n
     } result = Some(candidate)
+
+    result.getOrElse(Seq())
+  }
+
+  def reconstruirCadenaIngenuaPar(n: Int, oraculo: Oraculo): Seq[Char] = {
+    def generateAll(length: Int): Seq[Seq[Char]] = {
+      if (length == 0) Seq(Seq())
+      else for {
+        shorter <- generateAll(length - 1)
+        letter <- alfabeto
+      } yield shorter :+ letter
+    }
+
+    var result: Option[Seq[Char]] = None
+
+    for {
+      len <- 1 to n
+      if result.isEmpty
+    } {
+      val candidates = generateAll(len)
+      val tasks = candidates.map(c => task(oraculo(c)))
+
+      for ((c, t) <- candidates.zip(tasks)
+           if result.isEmpty && c.length == n && t.join()) {
+        result = Some(c)
+      }
+    }
 
     result.getOrElse(Seq())
   }
--- src/main/scala/ReconstCadenas/pruebas.sc
+++ src/main/scala/ReconstCadenas/pruebas.sc
@@ -1,7 +1,8 @@
 import Oraculo._
 import ReconstCadenas._
 
-val seq = List('a', 'c', 'c', 'a')
+val seq = List('g', 'c', 'g', 'a')
 val oraculo = crearOraculo(1)(seq)
 
 reconstruirCadenaIngenua(4, oraculo)
+reconstruirCadenaIngenuaPar(4, oraculo)
