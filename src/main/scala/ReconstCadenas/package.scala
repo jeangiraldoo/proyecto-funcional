fea68593b30f4758a58343d9ca8cc66af081a308
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2025-06-05 14:32:52 -0500

    turbo

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -48,4 +48,40 @@ package object ReconstCadenas {
 
     result.getOrElse(Seq())
   }
+
+  def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
+    require(n > 0 && (n & (n - 1)) == 0, "n debe ser una potencia de 2")
+    
+    def productoCartesiano(sc1: Seq[Seq[Char]], sc2: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+      for {
+        s1 <- sc1
+        s2 <- sc2
+      } yield s1 ++ s2
+    }
+
+    
+    def buscarCadena(sc: Seq[Seq[Char]], k: Int): Seq[Char] = {
+      if (k > n) {
+        throw new IllegalStateException("No se encontró la cadena")
+      } else {
+        val scK = if (k == 1) {
+          alfabeto.map(c => Seq(c))
+        } else {
+          productoCartesiano(sc, sc)
+        }
+        
+        val candidatasValidas = scK.filter(o)
+        
+        candidatasValidas.find(_.length == n) match {
+          case Some(cadenaEncontrada) => cadenaEncontrada
+          case None =>
+            buscarCadena(candidatasValidas, k * 2)
+        }
+      }
+    }
+
+    
+    val sc1 = alfabeto.map(c => Seq(c)).filter(o)
+    buscarCadena(sc1, 2)
+  }
 }
