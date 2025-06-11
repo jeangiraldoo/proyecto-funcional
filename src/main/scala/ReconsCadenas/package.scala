dd8252dfb2d34cf0912025b09012b516e8827696
Jean Giraldo <jeann.giraldo@gmail.com>
2025-06-10 22:54:38 -0500

    refactor: remove var usage in reconstruirCadenaIngenua

--- src/main/scala/ReconsCadenas/package.scala
+++ src/main/scala/ReconsCadenas/package.scala
@@ -3,7 +3,7 @@ import Common ._
 
 package object ReconsCadenas {
   val alfabeto = Seq('a', 'c', 'g', 't')
-  
+
   def reconstruirCadenaIngenua(n: Int, oraculo: Oraculo): Seq[Char] = {
     def generateAll(length: Int): Seq[Seq[Char]] = {
       if (length == 0) Seq(Seq())
@@ -12,14 +12,11 @@ package object ReconsCadenas {
         letter <- alfabeto
       } yield shorter :+ letter
     }
-    var result: Option[Seq[Char]] = None
-    for {
-      len <- 1 to n
-      candidate <- generateAll(len)
-      if result.isEmpty && oraculo(candidate) && candidate.length == n
-    } result = Some(candidate)
 
-    result.getOrElse(Seq())
+    (1 to n)
+      .flatMap(generateAll)
+      .find(candidate => oraculo(candidate) && candidate.length == n)
+      .getOrElse(Seq())
   }
 
   def reconstruirCadenaIngenuaPar(n: Int, oraculo: Oraculo): Seq[Char] = {
