1840026124db2cc167731050b4d92fc869454fbd
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2025-07-18 12:59:38 -0500

    turbo corregida

--- src/main/scala/ReconsCadenas/package.scala
+++ src/main/scala/ReconsCadenas/package.scala
@@ -47,38 +47,29 @@ package object ReconsCadenas {
   }
 
   def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
-    require(n > 0 && (n & (n - 1)) == 0, "n debe ser una potencia de 2")
-    
-    def productoCartesiano(sc1: Seq[Seq[Char]], sc2: Seq[Seq[Char]]): Seq[Seq[Char]] = {
-      for {
-        s1 <- sc1
-        s2 <- sc2
-      } yield s1 ++ s2
-    }
+    require(n > 0 && (n & (n - 1)) == 0)
 
-    
     def buscarCadena(sc: Seq[Seq[Char]], k: Int): Seq[Char] = {
-      if (k > n) {
-        throw new IllegalStateException("No se encontró la cadena")
+      val scK = for {
+        s1 <- sc
+        s2 <- sc
+      } yield s1 ++ s2
+
+      val candidatasValidas = scK.filter(o)
+
+      if (k == n) {
+        candidatasValidas.find(cadena => cadena.length == n && o(cadena)).get
       } else {
-        val scK = if (k == 1) {
-          alfabeto.map(c => Seq(c))
-        } else {
-          productoCartesiano(sc, sc)
-        }
-        
-        val candidatasValidas = scK.filter(o)
-        
-        candidatasValidas.find(_.length == n) match {
-          case Some(cadenaEncontrada) => cadenaEncontrada
-          case None =>
-            buscarCadena(candidatasValidas, k * 2)
-        }
+        buscarCadena(candidatasValidas, k * 2)
       }
     }
 
-    
     val sc1 = alfabeto.map(c => Seq(c)).filter(o)
-    buscarCadena(sc1, 2)
+
+    if (n == 1) {
+      sc1.head
+    } else {
+      buscarCadena(sc1, 2)
+    }
   }
 }
--- src/main/scala/ReconsCadenas/pruebas.sc
+++ src/main/scala/ReconsCadenas/pruebas.sc
@@ -4,5 +4,6 @@ import ReconsCadenas._
 val seq = List('g', 'c', 'g', 'a')
 val oraculo = crearOraculo(1)(seq)
 
-reconstruirCadenaIngenua(4, oraculo)
-reconstruirCadenaIngenuaPar(4, oraculo)
+
+
+reconstruirCadenaTurbo(4, oraculo)
